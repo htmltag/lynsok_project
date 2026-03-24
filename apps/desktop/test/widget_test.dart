fa4279490f838951a71444e66fc12e6662728bd6
Htmltag <jonathansoylandlier@gmail.com>
2026-03-24 21:43:23 +0100

    small naming changes. Update test

--- apps/desktop/test/widget_test.dart
+++ apps/desktop/test/widget_test.dart
@@ -15,16 +15,8 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const LynSokDesktopApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // TODO: Update this test to reflect the actual UI of LynSok.
+    // For example, verify that a Search Bar exists:
+    // expect(find.byType(TextField), findsOneWidget);
   });
 }
